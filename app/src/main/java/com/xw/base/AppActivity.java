02921d6448accc51eac98e94aede70e781e99bee
WxHlin <wei18627055053@sina.com>
2017-06-29 17:07:51 +0800

    封装BaseActivity、BaseFragment

---
+++ app/src/main/java/com/xw/base/AppActivity.java
@@ -0,0 +1,48 @@
+package com.xw.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/6/29 0029.
+ */
+
+public abstract class AppActivity extends BaseActivity{
+
+
+    //获取第一个fragment
+    protected abstract BaseFragment getFirstFragment();
+
+    //获取Intent
+    protected void handleIntent(Intent intent) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewId());
+        if (null != getIntent()) {
+            handleIntent(getIntent());
+        }
+        //避免重复添加Fragment
+        if (null == getSupportFragmentManager().getFragments()) {
+            BaseFragment firstFragment = getFirstFragment();
+            if (null != firstFragment) {
+                addFragment(firstFragment);
+            }
+        }
+
+    }
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_base;
+    }
+
+    @Override
+    protected int getFragmentContentId() {
+        return R.id.fragment_container;
+    }
+
+}
